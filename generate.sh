7e9c955b180ad90fac70166fb0f6b9c64908fc0d
John Silva <minemidnightmc@gmail.com>
2022-11-07 20:01:26 +0000

    update generate script

--- generate.sh
+++ generate.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
 # generate python
-protoc -I=. --python_out=generated main.proto
-protoc -I=. --python_out=generated hardwareMessages.proto
-protoc -I=. --python_out=generated simulationMessages.proto
+protoc --python_out=generated main.proto hardwareMessages.proto simulationMessages.proto
 
 # generate csharp
-protoc -I=. --csharp_out=generated main.proto
-protoc -I=. --csharp_out=generated hardwareMessages.proto
-protoc -I=. --csharp_out=generated simulationMessages.proto
+protoc --csharp_out=generated main.proto hardwareMessages.proto simulationMessages.proto
